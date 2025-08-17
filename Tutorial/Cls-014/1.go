f02e5b9378834eae59a3fdbf3c2876c348710c7f
mainurrahat <mainurrahat51@gmail.com>
2025-08-18 00:01:27 +0600

    scope reading

---
+++ Tutorial/Cls-014/1.go
@@ -0,0 +1,18 @@
+package main
+import "fmt"
+var (
+	a=20
+	b=30
+)
+func add(x int, y int)  {
+	z:=x+y
+	fmt.Println("Sum is:", z)
+}
+func main() {
+	p:=30
+	q:=40
+	add(p, q)
+	add(a, b)
+	add(a, p)
+}
+// scope jinis ta ki?
